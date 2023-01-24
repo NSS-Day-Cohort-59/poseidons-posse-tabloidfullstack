690e232f593531059a5bcf974dfd1946c8c657ad
Joshua Johns <joshuajohns28@outlook.com>
2023-01-24 13:03:16 -0600

    added list of posts

---
+++ Tabloid/client/src/modules/postManager.js
@@ -0,0 +1,24 @@
+import { getToken } from "./authManager";
+
+const _postUrl = "/api/post";
+
+
+export const getAllPosts = () => {
+    return getToken().then((token) => {
+        return fetch(`${_postUrl}`, {
+            method: "GET",
+            heafers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw new Error(
+                        "An unknown error occurred while trying to get posts.",
+                    );
+                }
+            });
+    });
+};
