58ac6475ae959b3136b1e1467dc567304745706b
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2023-01-26 13:26:23 -0600

    committed

--- Tabloid/Controllers/PostController.cs
+++ Tabloid/Controllers/PostController.cs
@@ -44,6 +44,7 @@ namespace Tabloid.Controllers
             _postRepository.Add(post);
             return CreatedAtAction("Get", new { id = post.Id }, post);
         }
+
         [HttpGet("myPosts/{firebaseUserId}")]
         public IActionResult GetByFirebaseUserId(string firebaseUserId)
         {
